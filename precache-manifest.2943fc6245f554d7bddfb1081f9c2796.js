fbc8fd760901ce0327ce6a1c743a366319868298
Vitor <vitorvieira.antunes@gmail.com>
2020-08-25 10:51:49 -0400

    Updates

---
+++ precache-manifest.2943fc6245f554d7bddfb1081f9c2796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863707b66353ee9ca879f3734275f7e5",
+    "url": "/vitorvieiraantunes/ReactTutorial/index.html"
+  },
+  {
+    "revision": "ec6591ce0a2860844118",
+    "url": "/vitorvieiraantunes/ReactTutorial/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "54ec31bc2eed5ff3e9c3",
+    "url": "/vitorvieiraantunes/ReactTutorial/static/js/2.69e85535.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vitorvieiraantunes/ReactTutorial/static/js/2.69e85535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6591ce0a2860844118",
+    "url": "/vitorvieiraantunes/ReactTutorial/static/js/main.8d96d969.chunk.js"
+  },
+  {
+    "revision": "58fd95006464a1fce665",
+    "url": "/vitorvieiraantunes/ReactTutorial/static/js/runtime-main.4f6ab9f3.js"
+  }
+]);
